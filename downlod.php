57522099dad497029ca6250c5f76eac9fe8a969f
ANKSUH <149316301+Aank169@users.noreply.github.com>
2023-11-03 17:10:15 +0530

    Update downlod.php

--- downlod.php
+++ downlod.php
@@ -4,37 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PDF Viewer and Downloader</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            text-align: center;
-            padding: 20px;
-        }
-
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            background-color: #fff;
-            border-radius: 8px;
-            padding: 20px;
-        }
-
-        h1 {
-            font-size: 24px;
-        }
-
-        .pdf-viewer {
-            text-align: center;
-        }
-
-        .pdf-link {
-            display: block;
-            margin-bottom: 20px;
-            text-decoration: none;
-            color: #007BFF;
-        }
-    </style>
 </head>
 <body>
     <div class="container">
